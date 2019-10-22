7263c5c0c5e12bd93095c7a097cad82fd130c31c
alemupx <alemupx@gmail.com>
2019-10-22 07:44:29 -0500

    Cambio color barraLateral

--- application/views/layouts/barraLateral.php
+++ application/views/layouts/barraLateral.php
@@ -1,5 +1,5 @@
 <div class="bg-light border-right" id="sidebar-wrapper">
-    <div class="sidebar-heading">Domotic Home </div>
+    <div class="sidebar-heading"><a style="color:crimson">Domotic Home</a></div>
     <div class="list-group list-group-flush">
         <a href="<?php echo base_url() ?>ambiente" class="list-group-item list-group-item-action bg-light">Ambientes</a>
         <a href="<?php echo base_url() ?>usuario" class="list-group-item list-group-item-action bg-light">Usuarios</a>
